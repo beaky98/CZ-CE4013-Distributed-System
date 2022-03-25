7bc9e0a89a41163de1970159afc761d12c203c69
ajykoshy <ajay0012@e.ntu.edu.sg>
2022-03-25 21:03:48 +0800

    changes to services and account

--- src/Server/Services.java
+++ src/Server/Services.java
@@ -12,7 +12,7 @@ public class Services {
 
 
 
-    public static int createAccount(String name, int pin, double balance){
+    public static int createAccount(String name, String pw, double balance){
         int accNum = -1;
 		int min = 1000;
 		int max = 9999;
@@ -24,7 +24,7 @@ public class Services {
 
         newAcc.setAccountNumber(accNum);
         newAcc.setName(name);
-        newAcc.setPin(pin);
+        newAcc.setPassword(pw);
         newAcc.setBalance(balance);
         accountdb.put(accNum,newAcc);
 
@@ -32,14 +32,16 @@ public class Services {
         return accNum;
     }
 
-    public static double updateBalance(String name, int accNum, int pin, int choice, double amount){
+    public static String updateBalance(String name, int accNum, String password, int choice, double amount){
+        String response = "";
+
         //Check if account number exists in database
         if(accountdb.get(accNum) == null){
-            return -1;
+            response = "Account does not exist";
         }
         //Check if pin is correct
-        if(accountdb.get(accNum).getPin() != pin){
-            return -2;
+        if(accountdb.get(accNum).getPassword() != password){
+            response = "Incorrect password, please try again";
         }
 
         if(choice == 0){
@@ -48,6 +50,8 @@ public class Services {
 
             Account temp = accountdb.get(accNum);
 			temp.setBalance(temp.getBalance() + amount);
+
+            response = "Deposit for account number " + accNum + " successful\n Balance is " + temp.getBalance() + " " + temp.getCurrency();
         }
         
         else if(choice == 1){
@@ -57,16 +61,90 @@ public class Services {
 
             if(temp.getBalance() >= amount){
 				temp.setBalance(temp.getBalance() - amount);
-				System.out.println("Withdrawal successful\nAccount's balance is " + temp.getBalance());
+				response = "Withdrawal for account number" + accNum + " successful\n Balance is " + temp.getBalance() + " " + temp.getCurrency();
 			}		
 			else {
-				System.out.println("Insuffient Balance. Current balance: " + temp.getBalance());
-				return -3;
+				response = "Insuffient Balance. Current balance: " + temp.getBalance();
 			}
         }        
-        else return -4;
+        else response = "Invalid choice, try again.";
 
-        return accountdb.get(accNum).getBalance();
+
+        return response;
         
+    }
+
+
+    public static String checkBalance(int accNum, String pw){
+        String response = "";
+
+        Account temp = accountdb.get(accNum);
+
+        if(temp == null){
+            response = "Account does not exist"; 
+        }
+        else{
+            if(temp.getPassword() == pw){
+                response = "Your balance is "+ temp.getBalance() + " " + temp.getCurrency();
+            }
+            else{
+                response = "Incorrect password, please try again.";
+            }
+        }
+        return response;
+    }
+
+    public static String closeAccount(String name, int accNum, String pw){
+        String response = "";
+        Account temp = accountdb.get(accNum);
+        if(temp == null){
+            response = "Unable to close account, account does not exist.";  
+        }
+        else{
+            if(temp.getPassword() == pw){
+                accountdb.remove(accNum);
+                response = "Account " + accNum + " successfully removed.";
+            }
+            else{
+                response = "Incorrect password, please try again.";
+            }
+        }
+        return response;
+    }
+
+    public static String transferBalance(String name, int accNum, int rec, String pw, double amount){
+        String response = "";
+        
+        Account sender, receiver;
+
+        if(accountdb.get(accNum) == null){
+            if(accountdb.get(rec) == null){
+                response = "Sender and Receiver account numbers, " + accNum + " and " + rec + ", do not exist";
+            }
+            else{
+                response = "Sender's account number " + accNum + " does not exist";
+            }
+        }
+        else if(accountdb.get(rec) == null){
+            response = "Receiver's account number " + rec + " does not exist";
+        }
+        else{
+            sender = accountdb.get(accNum);
+            receiver = accountdb.get(rec);
+
+            if(sender.getPassword() != pw){
+                response = "Incorrect password, please try again.";
+            }
+
+            else if(sender.getBalance() >= amount){
+                sender.setBalance(sender.getBalance() - amount);
+                receiver.setBalance(receiver.getBalance() + amount);
+                response = "Successful transfer \n Your balance is now " + sender.getBalance() + " " + sender.getCurrency();
+            }
+            else{
+                response = "Insufficient funds,  your balance is " + sender.getBalance() + " " + sender.getCurrency();
+            }
+        }
+        return response;
     }
 }
